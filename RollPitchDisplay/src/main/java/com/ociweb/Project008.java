f863240d4e05f670bb1f0df060d5bbe554ed8bb9
hendrix2222 <hendrix2222@yahoo.com>
2018-01-23 09:22:49 -0600

    Adds initial commit of RollPitchDisplay project.

---
+++ RollPitchDisplay/src/main/java/com/ociweb/Project008.java
@@ -0,0 +1,32 @@
+package com.ociweb;
+
+
+import com.ociweb.iot.maker.*;
+import static com.ociweb.iot.maker.Port.*;
+
+public class Project008 implements FogApp
+{
+    ///////////////////////
+    //Connection constants
+    ///////////////////////
+
+
+    @Override
+    public void declareConnections(Hardware hardware) {
+        ////////////////////////////
+        //Connection specifications
+        ///////////////////////////
+
+        
+    }
+
+
+    @Override
+    public void declareBehavior(FogRuntime runtime) {
+        //////////////////////////////
+        //Specify the desired behavior
+        //////////////////////////////
+
+    }
+          
+}
